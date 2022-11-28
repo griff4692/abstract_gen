f7c7fa5177bbd4fab7315ba0b9de2673c9ceef40
Griffin Adams <griff4692@gmail.com>
2022-11-28 09:41:19 -0500

    Update lc_inference to look for the_chosen_one first

--- abstract/model/lc_inference.sh
+++ abstract/model/lc_inference.sh
@@ -7,41 +7,44 @@ EXPERIMENT=$3
 METRIC=$4
 
 DIR="${HOME}/data_tmp/weights/$EXPERIMENT"
+BEST_STEP_FN="${DIR}/the_chosen_one.txt"
 INFERENCE_BATCH_SIZE=8
 MAX_EXAMPLES=99999999
 
-SPLIT="validation"
-for STEP in 1000 2000 3000 4000 5000 6000 7000 8000 9000 10000
-do
-  CKPT_NAME="ckpt_${STEP}_steps"
-  STEP_DIR="${DIR}/${CKPT_NAME}"
+if [ ! -f $BEST_STEP_FN ]
+then
+  SPLIT="validation"
+  for STEP in 1000 2000 3000 4000 5000 6000 7000 8000 9000 10000
+  do
+    CKPT_NAME="ckpt_${STEP}_steps"
+    STEP_DIR="${DIR}/${CKPT_NAME}"
 
-  if [ ! -d $STEP_DIR ]
-  then
-    echo "$STEP_DIR does not exist. Skipping."
-    continue
-  fi
+    if [ ! -d $STEP_DIR ]
+    then
+      echo "$STEP_DIR does not exist. Skipping."
+      continue
+    fi
 
-  OUT_MODEL_FN="${STEP_DIR}/pytorch_model.bin"
-  if [ ! -f $OUT_MODEL_FN ]
-  then
-    python "$STEP_DIR/zero_to_fp32.py" $STEP_DIR $OUT_MODEL_FN
-  fi
-  python inference.py --hf_model primera --device $DEVICE --experiment $EXPERIMENT --dataset $DATASET --batch_size $INFERENCE_BATCH_SIZE --ckpt_name $CKPT_NAME --results_name $CKPT_NAME --max_examples $MAX_EXAMPLES --split $SPLIT
+    OUT_MODEL_FN="${STEP_DIR}/pytorch_model.bin"
+    if [ ! -f $OUT_MODEL_FN ]
+    then
+      python "$STEP_DIR/zero_to_fp32.py" $STEP_DIR $OUT_MODEL_FN
+    fi
+    python inference.py --hf_model primera --device $DEVICE --experiment $EXPERIMENT --dataset $DATASET --batch_size $INFERENCE_BATCH_SIZE --ckpt_name $CKPT_NAME --results_name $CKPT_NAME --max_examples $MAX_EXAMPLES --split $SPLIT
+    cd ../eval
+    OUT_FN="${STEP_DIR}/${SPLIT}_predictions.csv"
+    CUDA_VISIBLE_DEVICES=$DEVICE bash run_all.sh $DATASET $OUT_FN $METRIC
+    cd ../model
+  done
+
+  echo "Fini generating for validation set! Finding best validation set"
+
   cd ../eval
   OUT_FN="${STEP_DIR}/${SPLIT}_predictions.csv"
-  CUDA_VISIBLE_DEVICES=$DEVICE bash run_all.sh $DATASET $OUT_FN $METRIC
+  python find_best_val_ckpt.py --dataset $DATASET --experiment $EXPERIMENT --metric $METRIC
   cd ../model
-done
+fi
 
-echo "Fini generating for validation set! Finding best validation set"
-
-cd ../eval
-OUT_FN="${STEP_DIR}/${SPLIT}_predictions.csv"
-python find_best_val_ckpt.py --dataset $DATASET --experiment $EXPERIMENT --metric $METRIC
-cd ../model
-
-BEST_STEP_FN="${DIR}/the_chosen_one.txt"
 while read BEST_STEP; do
   echo "${BEST_STEP} is the best step on the validation set."
 done < $BEST_STEP_FN
--- abstract/model/remove_unused.py
+++ abstract/model/remove_unused.py
@@ -4,10 +4,21 @@ from glob import glob
 if __name__ == '__main__':
     fns = list(glob(os.path.expanduser('~/data_tmp/weights/*/*/pytorch_model/*.pt')))
 
-    just_optim = True
+    # just_optim = True
     for fn in fns:
-        if just_optim and 'optim' not in fn:
-            continue
+        print(fn)
+    #     if just_optim and 'optim' not in fn:
+    #         continue
+    #
+    #     print(f'Removing {fn}')
+    #     os.remove(fn)
 
+    bests = list(glob(os.path.expanduser('~/data_tmp/weights/*/best_ckpt/pytorch_model/*.pt')))
+    for fn in bests:
+        print(f'Removing {fn}')
+        os.remove(fn)
+
+    lasts = list(glob(os.path.expanduser('~/data_tmp/weights/*/last_ckpt/pytorch_model/*.pt')))
+    for fn in lasts:
         print(f'Removing {fn}')
         os.remove(fn)
