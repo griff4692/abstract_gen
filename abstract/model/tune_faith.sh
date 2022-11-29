86a526ff879b6cd0f95a3d8b7c06169a4bacf3b3
Griffin Adams <griff4692@gmail.com>
2022-11-28 21:56:40 -0500

    Fix

--- abstract/model/tune_faith.sh
+++ abstract/model/tune_faith.sh
@@ -26,11 +26,11 @@ ACCELERATE_CMD="accelerate launch --main_process_port=$PORT --mixed_precision=fp
 NUM_POS=2
 NUM_NEG=2
 
-PROGRAM_ARGS="--exit_after_n_steps $TUNE_STEPS --validate_every_n_steps $TUNE_STEPS --reference_status positive -contrast --contrast_ckpt $CONTRAST_CKPT --max_num_positive $NUM_POS --max_num_negative $NUM_NEG --positive_methods $POS_METHODS --negative_methods $NEG_METHODS --contrast_objective $OBJECTIVE --max_target_length $MAX_TARGET_LENGTH --contrast_metrics $METRICS --gradient_accumulation_steps $GRAD_ACCUM --dataset $DATASET --hf_model $HF_MODEL --validate_every_n_steps $STEPS_PER_VALIDATION --max_train_steps $MAX_STEPS"
+PROGRAM_ARGS="--exit_after_n_steps $TUNE_STEPS -save_every_time --validate_every_n_steps $TUNE_STEPS --contrast_intra_sample_strategy random --reference_status positive -contrast --contrast_ckpt $CONTRAST_CKPT --max_num_positive $NUM_POS --max_num_negative $NUM_NEG --positive_methods $POS_METHODS --negative_methods $NEG_METHODS --contrast_objective $OBJECTIVE --max_target_length $MAX_TARGET_LENGTH --contrast_metrics $METRICS --gradient_accumulation_steps $GRAD_ACCUM --dataset $DATASET --hf_model $HF_MODEL --validate_every_n_steps $STEPS_PER_VALIDATION --max_train_steps $MAX_STEPS"
 
 echo $ACCELERATE_CMD
 echo $PROGRAM_ARGS
-$ACCELERATE_CMD $PROGRAM_ARGS --experiment faith_medium_mle --contrast_intra_sample_strategy $SAMPLE_STRATEGY -save_every_time --mle_weight 1.0
-$ACCELERATE_CMD $PROGRAM_ARGS --experiment faith_high_mle --contrast_intra_sample_strategy $SAMPLE_STRATEGY -save_every_time --mle_weight 2.0
-$ACCELERATE_CMD $PROGRAM_ARGS --experiment faith_highest_mle --contrast_intra_sample_strategy $SAMPLE_STRATEGY -save_every_time --mle_weight 10.0
-$ACCELERATE_CMD $PROGRAM_ARGS --experiment faith_low_mle --contrast_intra_sample_strategy $SAMPLE_STRATEGY -save_every_time --mle_weight 0.1
+$ACCELERATE_CMD $PROGRAM_ARGS --mle_weight 1.0 --experiment tune_faith_medium_mle
+$ACCELERATE_CMD $PROGRAM_ARGS --mle_weight 2.0 --experiment tune_faith_high_mle
+$ACCELERATE_CMD $PROGRAM_ARGS --mle_weight 10.0 --experiment tune_faith_highest_mle
+$ACCELERATE_CMD $PROGRAM_ARGS --experiment faith_low_mle --mle_weight 0.1
