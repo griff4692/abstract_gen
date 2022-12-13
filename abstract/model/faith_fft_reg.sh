3d30bc4306e4ba58b3e99ad3cbea2e478992aab0
Griffin Adams <griff4692@gmail.com>
2022-12-13 15:41:16 -0500

    Lower max target length for faith fft.

--- abstract/model/faith_fft_reg.sh
+++ abstract/model/faith_fft_reg.sh
@@ -17,7 +17,6 @@ POS_METHODS="all"
 NEG_METHODS="all"
 HF_MODEL="primera"
 CONTRAST_CKPT="primera_ft_$DATASET"
-MAX_TARGET_LENGTH=512
 STEPS_PER_VALIDATION=1000
 MAX_STEPS=10000
 
@@ -30,15 +29,18 @@ then
   MLE_WEIGHT=1.0
   CONTRAST_WEIGHT=1.0
   REF_STATUS="positive"
+  MAX_TARGET_LENGTH=256
 elif [[ $DATASET == "chemistry" ]]
 then
   MLE_WEIGHT=1.0
   CONTRAST_WEIGHT=10.0
   REF_STATUS="positive"
+  MAX_TARGET_LENGTH=384
 else
   MLE_WEIGHT=1.0
   CONTRAST_WEIGHT=1.0
   REF_STATUS="ensure"
+  MAX_TARGET_LENGTH=384
 fi
 
 PROGRAM_ARGS="--per_device_eval_batch_size 4 --mle_weight $MLE_WEIGHT --contrast_weight $CONTRAST_WEIGHT -save_every_time --reference_status $REF_STATUS -contrast --contrast_ckpt $CONTRAST_CKPT --max_num_rank $NUM_CAND --max_num_positive $NUM_POS --max_num_negative $NUM_NEG --positive_methods $POS_METHODS --negative_methods $NEG_METHODS --contrast_objective ${OBJECTIVE} --max_target_length $MAX_TARGET_LENGTH --contrast_metrics $METRICS --gradient_accumulation_steps $GRAD_ACCUM --dataset $DATASET --hf_model $HF_MODEL --validate_every_n_steps $STEPS_PER_VALIDATION --max_train_steps $MAX_STEPS"
