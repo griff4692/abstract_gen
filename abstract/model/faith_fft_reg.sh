ed11a0124e34ea7a273862974c566ba334984977
Griffin Adams <griff4692@gmail.com>
2022-12-13 16:18:32 -0500

    Minor

--- abstract/model/faith_fft_reg.sh
+++ abstract/model/faith_fft_reg.sh
@@ -41,7 +41,7 @@ else
   MLE_WEIGHT=1.0
   CONTRAST_WEIGHT=1.0
   REF_STATUS="ensure"
-  MAX_TARGET_LENGTH=384
+  MAX_TARGET_LENGTH=512
 fi
 
 PROGRAM_ARGS="--per_device_eval_batch_size $EVAL_BS --mle_weight $MLE_WEIGHT --contrast_weight $CONTRAST_WEIGHT -save_every_time --reference_status $REF_STATUS -contrast --contrast_ckpt $CONTRAST_CKPT --max_num_rank $NUM_CAND --max_num_positive $NUM_POS --max_num_negative $NUM_NEG --positive_methods $POS_METHODS --negative_methods $NEG_METHODS --contrast_objective ${OBJECTIVE} --max_target_length $MAX_TARGET_LENGTH --contrast_metrics $METRICS --gradient_accumulation_steps $GRAD_ACCUM --dataset $DATASET --hf_model $HF_MODEL --validate_every_n_steps $STEPS_PER_VALIDATION --max_train_steps $MAX_STEPS"
