a00ca42040bb3ff8f6a551aaccf60add5580340c
Griffin Adams <griff4692@gmail.com>
2022-12-13 11:53:54 -0500

    More

--- abstract/model/bulk_pubmed_eval.sh
+++ abstract/model/bulk_pubmed_eval.sh
@@ -2,12 +2,13 @@
 set -e
 
 EXPS="pubmed_min_metric_rel pubmed_max_metric_rel pubmed_max_margin_rel pubmed_min_margin_rel pubmed_max_diversity_rel"
+CKPT="ckpt_1000_steps"
 
 for EXP in $EXPS
 do
-  python inference.py --experiment $EXP --dataset pubmed --split test --max_examples 16 --device $1
+  python inference.py --experiment $EXP --dataset pubmed --split test --max_examples 16 --device $1 --ckpt_name $CKPT --results_name $CKPT
   cd ../eval
-  FN="$HOME/weights/$EXP/ckpt_1000_steps/test_predictions.csv"
+  FN="$HOME/weights/$EXP/$CKPT/test_predictions.csv"
   CUDA_VISIBLE_DEVICES=$1 bash run_all.sh pubmed $FN all
   cd ../model
 done
