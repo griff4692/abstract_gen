67b78bdc57bc06180e8c4751d0a05cc2d8ef738a
Griffin Adams <griff4692@gmail.com>
2022-12-15 09:27:46 -0500

    minor

--- abstract/model/download_weight.sh
+++ abstract/model/download_weight.sh
@@ -3,5 +3,5 @@ set -e
 
 TO_DIR=$HOME/data_tmp/weights
 echo "Download experiment $1 to $TO_DIR"
-~/azcopy_linux_amd64_10.16.2/azcopy copy --recursive=true --from-to=BlobLocal "https://sustainsysdata.blob.core.windows.net/recipe-gen/weights/${1}?${SAS_TOKEN}" $TO_DIR
+~/azcopy copy --recursive=true --from-to=BlobLocal "https://sustainsysdata.blob.core.windows.net/recipe-gen/weights/${1}?${SAS_TOKEN}" $TO_DIR
 echo "Fini!"
