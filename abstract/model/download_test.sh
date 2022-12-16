b175af98d6fba9ba8e1c7306f5039f7a7714b84a
Griffin Adams <griff4692@gmail.com>
2022-12-16 13:39:09 -0500

    More

--- abstract/model/download_test.sh
+++ abstract/model/download_test.sh
@@ -11,6 +11,6 @@ do
   STEP_DIR=weights/${1}/ckpt_${step}_steps
   TO_DIR=$HOME/data_tmp/$STEP_DIR
   echo "Download test predictions from ${STEP_DIR} to ${TO_DIR}"
-  ~/azcopy copy --recursive=true --from-to=BlobLocal "https://sustainsysdata.blob.core.windows.net/recipe-gen/${STEP_DIR}/test_predictions.csv?${SAS_TOKEN}" $TO_DIR
+  ~/azcopy copy --from-to=BlobLocal "https://sustainsysdata.blob.core.windows.net/recipe-gen/${STEP_DIR}/test_predictions.csv?${SAS_TOKEN}" $TO_DIR
 done
 echo "Fini!"
