f5abcd6a62f1be8dd7f5292bcac27f1e457dba35
Griffin Adams <griff4692@gmail.com>
2022-12-16 13:46:59 -0500

    More

--- abstract/model/download_test.sh
+++ abstract/model/download_test.sh
@@ -8,9 +8,9 @@ fi
 
 for step in "${@:2}"
 do
-  STEP_DIR=weights/${1}/ckpt_${step}_steps
-  TO_DIR=$HOME/data_tmp/$STEP_DIR
-  echo "Download test predictions from ${STEP_DIR} to ${TO_DIR}"
-  ~/azcopy copy --from-to=BlobLocal "https://sustainsysdata.blob.core.windows.net/recipe-gen/${STEP_DIR}/test_predictions.csv?${SAS_TOKEN}" $TO_DIR
+  FROM_FN="weights/${1}/ckpt_${step}_steps/test_predictions.csv"
+  TO_FN="$HOME/data_tmp/${1}/ckpt_${step}_steps/test_predictions.csv"
+  echo "Download test predictions from ${FROM_FN} to ${TO_FN}"
+  ~/azcopy copy --from-to=BlobLocal "https://sustainsysdata.blob.core.windows.net/recipe-gen/${FROM_FN}?${SAS_TOKEN}" $TO_DIR
 done
 echo "Fini!"
