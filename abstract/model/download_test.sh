690ad67f66993be711c39908bca3cec8a41b6df5
Griffin Adams <griff4692@gmail.com>
2022-12-16 13:53:11 -0500

    Fixed downoad test

--- abstract/model/download_test.sh
+++ abstract/model/download_test.sh
@@ -9,7 +9,7 @@ fi
 for step in "${@:2}"
 do
   FROM_FN="weights/${1}/ckpt_${step}_steps/test_predictions.csv"
-  TO_FN="$HOME/data_tmp/${1}/ckpt_${step}_steps/test_predictions.csv"
+  TO_FN="$HOME/data_tmp/weights/${1}/ckpt_${step}_steps/test_predictions.csv"
   echo "Download test predictions from ${FROM_FN} to ${TO_FN}"
   ~/azcopy copy --from-to=BlobLocal "https://sustainsysdata.blob.core.windows.net/recipe-gen/${FROM_FN}?${SAS_TOKEN}" $TO_DIR
 done
